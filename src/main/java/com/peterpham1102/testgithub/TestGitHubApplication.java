c190ed8d4c4af656567fdcf15868bf1e7478a0e3
peterpham1102 <youngbuffalo1102@gmail.com>
2020-10-15 16:18:28 +0700

    Initial commit

---
+++ src/main/java/com/peterpham1102/testgithub/TestGitHubApplication.java
@@ -0,0 +1,13 @@
+package com.peterpham1102.testgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGitHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGitHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/peterpham1102/testgithub/TestGitHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peterpham1102.testgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestGitHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
